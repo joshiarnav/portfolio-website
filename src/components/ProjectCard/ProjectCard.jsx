bccee06c902d8cdcebff414d5bd761de2e93cb0c
joshiarnav <joshi.arnav.a@gmail.com>
2023-08-11 14:40:56 -0400

    adds live links to projects page

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -6,24 +6,28 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 const ProjectCard = (props) => {
-  const [experienceTitle, setExperienceTitle] = useState("Verisk");
-
-  function handleClick(e) {
-    setExperienceTitle(e.target.innerText);
-  }
-
   return (
     <div className="project-card">
       <Typography variant="h5">{props.title}</Typography>
       <Typography>{props.desc}</Typography>
       <div className="project-card-links">
         {props.github ? (
-          <IconButton href={props.github}>
+          <IconButton
+            href={props.github}
+            onClick={() => {
+              if (props.githubAlert) alert(props.githubAlert);
+            }}
+          >
             <GitHubIcon />
           </IconButton>
         ) : null}
         {props.link ? (
-          <IconButton href={props.link}>
+          <IconButton
+            href={props.link}
+            onClick={() => {
+              if (props.linkAlert) alert(props.linkAlert);
+            }}
+          >
             <OpenInNewIcon />
           </IconButton>
         ) : null}
